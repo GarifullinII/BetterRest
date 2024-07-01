fc89f363662b2ccd28d4864b4bff0201a91dbefe
GarifullinII <ildaridrisovich@gmail.com>
2024-07-01 15:19:21 +0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ildar Garifullin on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
